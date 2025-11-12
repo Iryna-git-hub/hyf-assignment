0c586a8306ae4ebcc42590f6c39919eeb681813e
irynalopatina <lopatina.iryna@gmail.com>
2025-11-13 00:06:52 +0100

    added Weather wear app

---
+++ JavaScript/week2/weatherApp.js
@@ -0,0 +1,30 @@
+// Based on the temperature the "Weather wear" returns a string with what the user should wear
+function weatherWear(temperature) {
+    // Initializing weather-appropriate clothing array
+  const clothes = [
+    "Warm coat, sweater, jeans, gloves, boots",
+    "Coat or puffer, sweater, jeans, hat",
+    "Light jacket, long sleeves, jeans, sneakers",
+    "Long sleeves, jeans, light shoes",
+    "T-shirt, jeans or shorts",
+  ];
+
+  // 
+  let weatherReadyClothes;
+  if (temperature > -10 && temperature < 0) {
+    weatherReadyClothes = clothes[0];
+  } else if (temperature >= 0 && temperature < 10) {
+    weatherReadyClothes = clothes[1];
+  } else if (temperature >= 10 && temperature < 15) {
+    weatherReadyClothes = clothes[2];
+  } else if (temperature >= 15 && temperature < 20) {
+    weatherReadyClothes = clothes[3];
+  } else if (temperature >= 20 && temperature < 30) {
+    weatherReadyClothes = clothes[4];
+  } else 
+    {weatherReadyClothes = "Stay at home!";}
+
+  console.log(weatherReadyClothes);
+}
+
+const clothesToWear = weatherWear(18);
