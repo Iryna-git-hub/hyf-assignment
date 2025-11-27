1299d2bd557d2e6ec4d27a959eb1350b4939cac8
irynalopatina <lopatina.iryna@gmail.com>
2025-11-27 13:21:24 +0100

    added Fibonacci Sequence exercise - solved during class session

---
+++ JavaScript/week4/exercise/session-exercise.js
@@ -0,0 +1,34 @@
+// Fibonacci Sequence
+function fib(position) {
+  if (position <= 0) {
+    console.error("Not allowed");
+    return NaN;
+  }
+  const values = [0, 1];
+
+  for (let i = 2; i < position; i++) {
+    const result = values[i - 1] + values[i - 2];
+    values.push(result);
+  }
+  console.log(values);
+  return values[position - 1];
+}
+
+const f = fib(-2);
+console.log(f);
+
+// Fizz buzz
+
+// Build a sentiment analyser
+
+// Credit card number formatter
+
+// Character frequencies
+
+// Palindromic substring
+
+// Credit card info
+
+// Tic Tac Toe
+
+// Conway's game of life
