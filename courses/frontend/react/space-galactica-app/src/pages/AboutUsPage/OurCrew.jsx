836a39fc2f4017a6fc18b1aea62eab672d3a6eed
irynalopatina <lopatina.iryna@gmail.com>
2026-04-15 13:10:12 +0200

    build: create separate components

---
+++ courses/frontend/react/space-galactica-app/src/pages/AboutUsPage/OurCrew.jsx
@@ -0,0 +1,103 @@
+import styles from "./OurCrew.module.css";
+
+export const OurCrew = () => {
+  return (
+    <>
+      <p>
+        Our crew is the heart and soul of Galactica. We are a diverse team of
+        seasoned space explorers, engineers, and visionaries who are united by a
+        common goal: to make space travel accessible and exciting for all.
+      </p>
+      <ul>
+        <li>
+          <article>
+            <img
+              src="../../public/crew/image-anousheh-ansari.png"
+              alt="Sarah Vega photo"
+            />
+            <header>
+              <h3>Sarah Vega</h3>
+              <p>Captain</p>
+            </header>
+            <p>
+              A former NASA astronaut with over 15 years of experience, Captain
+              Vega leads our missions with unparalleled expertise and a passion
+              for space exploration.
+            </p>
+          </article>
+        </li>
+        <li>
+          <article>
+            <img
+              src="../../public/crew/image-douglas-hurley.png"
+              alt="Dr. Leo Redding photo"
+            />
+            <header>
+              <h3>Dr. Leo Redding</h3>
+              <p>Chief astrophysicist</p>
+            </header>
+            <p>
+              Our chief astrophysicist, Dr. Redding, is a renowned scientist who
+              has contributed to major space discoveries. He ensures that every
+              journey is as educational as it is exhilarating.
+            </p>
+          </article>
+        </li>
+        <li>
+          <article>
+            <img
+              src="../../public/crew/image-victor-glover.png"
+              alt="Hana Lee photo"
+            />
+            <header>
+              <h3>Hana Lee</h3>
+              <p>Chief Engineer</p>
+            </header>
+            <p>
+              With her extensive background in aerospace engineering, Hana Lee
+              is responsible for the state-of-the-art technology that powers our
+              spacecraft. Her innovation ensures that our travelers are always
+              in safe hands.
+            </p>
+          </article>
+        </li>
+        <li>
+          <article>
+            <img
+              src="../../public/crew/image-mark-shuttleworth.png"
+              alt="Alex Santos photo"
+            />
+            <header>
+              <h3>Alex Santos</h3>
+              <p>Mission Specialist</p>
+            </header>
+            <p>
+              As a mission specialist, Alex’s job is to ensure that every aspect
+              of the journey runs smoothly. With a background in both science
+              and adventure tourism, Alex is the perfect guide for our space
+              travelers.
+            </p>
+          </article>
+        </li>
+        <li>
+          <article>
+            <img
+              src="../../public/crew/image-anousheh-ansari.png"
+              alt="Maya Patel photo"
+            />
+            <header>
+              <h3>Maya Patel</h3>
+              <p>Crew Member</p>
+            </header>
+            <p>
+              Maya brings a unique blend of technical skills and customer
+              service experience to the team. She’s always ready to assist with
+              any needs and to make sure every traveler has an unforgettable
+              experience.
+            </p>
+          </article>
+        </li>
+      </ul>
+    </>
+  );
+};
---
+++ courses/frontend/react/space-galactica-app/src/pages/AboutUsPage/OurPartners.jsx
@@ -0,0 +1,38 @@
+import styles from "./OurPartners.module.css";
+
+export const OurPartners = () => {
+  return (
+    <>
+      <p>
+        We collaborate with some of the most respected names in the space and
+        technology industries to make every journey extraordinary.
+      </p>
+      <ul>
+        <li>
+          <img src="../../public/business_partners/CBC_Logo_White.png" alt="" />
+        </li>
+        <li>
+          <img src="../../public/business_partners/CBC_Logo_White.png" alt="" />
+        </li>
+        <li>
+          <img src="../../public/business_partners/CBC_Logo_White.png" alt="" />
+        </li>
+        <li>
+          <img src="../../public/business_partners/CBC_Logo_White.png" alt="" />
+        </li>
+        <li>
+          <img src="../../public/business_partners/CBC_Logo_White.png" alt="" />
+        </li>
+        <li>
+          <img src="../../public/business_partners/CBC_Logo_White.png" alt="" />
+        </li>
+        <li>
+          <img src="../../public/business_partners/CBC_Logo_White.png" alt="" />
+        </li>
+        <li>
+          <img src="../../public/business_partners/CBC_Logo_White.png" alt="" />
+        </li>
+      </ul>
+    </>
+  );
+};
