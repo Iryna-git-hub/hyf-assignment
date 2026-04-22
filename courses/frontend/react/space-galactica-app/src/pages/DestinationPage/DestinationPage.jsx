7e66d44d6b2b2c5d173483fceb443fb472401006
irynalopatina <lopatina.iryna@gmail.com>
2026-04-22 23:41:35 +0200

    refactor: add planets with PlanetCard component

--- courses/frontend/react/space-galactica-app/src/pages/DestinationPage/DestinationPage.jsx
+++ courses/frontend/react/space-galactica-app/src/pages/DestinationPage/DestinationPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import styles from "./DestinationPage.module.css";
 import { PlanetsWishlistItem } from "./PlanetsWishlistItem";
+import { PlanetCard } from "./PlanetCard";
 
 export const Destinations = () => {
   const [planetsWishlist, setPlanetsWishlist] = useState([]);
@@ -35,6 +36,30 @@ export const Destinations = () => {
   const wishlistCount = planetsWishlist.length;
   const wishlistPlanetLabel = wishlistCount === 1 ? "planet" : "planets";
 
+  const destinationPlanets = [
+    {
+      name: "Europa",
+      description: "Europa, one of Jupiter’s moons, is an icy world with a hidden ocean beneath its surface. This mysterious moon is a prime candidate for the search for extraterrestrial life, making it a thrilling destination for space explorers.",
+      thumbnail: "/destination/image-europa.png"
+    },
+    {
+      name: "Mars",
+      description: "Mars, the Red Planet, is a barren yet fascinating world with vast deserts, towering volcanoes, and the deepest canyon in the solar system. As humanity’s next frontier, Mars invites us to dream of colonization and the possibilities of life beyond Earth.",
+      thumbnail: "/destination/image-mars.png"
+    },
+    {
+      name: "Moon",
+      description: "Our closest celestial neighbor, the Moon, is a silent witness to Earth's history. With its stunning craters and desolate landscapes, the Moon offers a unique glimpse into space exploration's past and future, making it a perfect destination for lunar adventurers.",
+      thumbnail: "/destination/image-moon.png"
+    },
+    {
+      name: "Titan",
+      description: "Titan, Saturn's largest moon, is a world of dense atmosphere and liquid methane lakes. This enigmatic moon is shrouded in a thick orange haze, concealing a landscape that is both alien and strangely familiar, beckoning explorers to uncover its secrets.",
+      thumbnail: "/destination/image-titan.png"
+    }
+
+  ];
+
   return (
     <div className="fullBGpicture">
       <main className="mainContent">
@@ -49,9 +74,6 @@ export const Destinations = () => {
             </p>
           )}
 
-          
-          
-
           {/* 🧑🏽‍🚀 Task - Week 3 */}
           {/* Use the AddWishlistItem component here. */}
 
@@ -65,48 +87,18 @@ export const Destinations = () => {
         </section>
         <section className="card">
           <h2>Possible destinations</h2>
-          {/* 🧑🏽‍🚀 Task - Week 2 */}
-          {/* Add all 4 planets: Europa, Moon, Mars, Titan.  */}
-          {/* Use the README.md file for descriptions. */}
-          {/* Create a <PlanetCard /> component, which accepts the following props: name, description, thumbnail, isSelected, togglePlanetSelection */}
-          <div className={styles.planetCard}>
-            <img
-              className={styles.planetThumbnail}
-              src="/destination/image-europa.png"
-              alt=""
-            />
-            <div className={styles.planetDescription}>
-              <h2>EUROPA {isPlanetInWishlist("Europa") ? "- SELECTED" : ""}</h2>
-              <p>Lorem ipsum...</p>
-            </div>
-            <button
-              className="roundButton"
-              onClick={() => togglePlanetSelection("Europa")}
-            >
-              {isPlanetInWishlist("Europa")
-                ? "REMOVE FROM WISHLIST"
-                : "ADD TO WISHLIST"}
-            </button>
-          </div>
-          <div className={styles.planetCard}>
-            <img
-              className={styles.planetThumbnail}
-              src="/destination/image-mars.png"
-              alt=""
-            />
-            <div className={styles.planetDescription}>
-              <h2>MARS {isPlanetInWishlist("Mars") ? "- SELECTED" : ""}</h2>
-              <p>Lorem ipsum...</p>
-            </div>
-            <button
-              className="roundButton"
-              onClick={() => togglePlanetSelection("Mars")}
-            >
-              {isPlanetInWishlist("Mars")
-                ? "REMOVE FROM WISHLIST"
-                : "ADD TO WISHLIST"}
-            </button>
-          </div>
+          { destinationPlanets.map(planet => (
+            <PlanetCard 
+            key={planet.name}
+            name={planet.name}
+            description={planet.description}
+            thumbnail={planet.thumbnail}
+            isSelected={isPlanetInWishlist(planet.name)}
+            togglePlanetSelection={togglePlanetSelection}
+          />
+          ))}
+          
+          
         </section>
       </main>
     </div>
