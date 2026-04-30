041add83cb19b2a18a079ca8000a44f9774cf733
irynalopatina <lopatina.iryna@gmail.com>
2026-04-30 20:44:01 +0200

    chore: add alt to a planet img

--- courses/frontend/react/space-galactica-app/src/pages/DestinationPage/PlanetsWishlistItem.jsx
+++ courses/frontend/react/space-galactica-app/src/pages/DestinationPage/PlanetsWishlistItem.jsx
@@ -3,7 +3,11 @@ import styles from "./DestinationPage.module.css";
 export const PlanetsWishlistItem = ({ name, thumbnail, onRemove }) => {
   return (
     <div className={styles.wishlistItem}>
-      <img className={styles.wishlistItemThumbnail} src={thumbnail} alt="" />
+      <img
+        className={styles.wishlistItemThumbnail}
+        src={thumbnail}
+        alt={`${name} planet`}
+      />
       <b>{name.toUpperCase()}</b>
       <button onClick={onRemove}>remove</button>
     </div>
